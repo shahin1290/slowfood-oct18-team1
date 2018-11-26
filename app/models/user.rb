495aaa9ccad1644df34df709f97b3a2de1cb9c9a
Jonathan <jon1969edwards@gmail.com>
2018-11-26 19:56:08 +0100

    Adds name, phone and address data to database and validates all as true in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
 end
