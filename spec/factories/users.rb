525ccd26549d75977ea7d0b87a27ea290de9d679
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-27 13:21:45 +0100

    Adds unit tests for user creation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    name {'Greg'}
+    email {'real@mail.com'}
+    phone {'123456789'}
+    address {'realaddress'}
+    password {'password'}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'db table' do
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :address }
+    it { is_expected.to have_db_column :phone }
+    it { is_expected.to have_db_column :encrypted_password }
+  end
+
+  describe FactoryBot do 
+    it 'should be valid' do
+      expect(FactoryBot.create(:user)).to be_valid
+    end
+  end
 end
