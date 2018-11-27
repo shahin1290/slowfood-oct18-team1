bc0f32063b74e48e8d0523cc42f6d96d3e672083
camron <camronmoradi@gmail.com>
2018-11-27 21:24:05 +0100

    Creates the Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_201256) do
+ActiveRecord::Schema.define(version: 2018_11_27_202129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dishes", force: :cascade do |t|
     t.string "name"
@@ -21,6 +27,9 @@ ActiveRecord::Schema.define(version: 2018_11_27_201256) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_dishes_on_category_id"
   end
 
+  add_foreign_key "dishes", "categories"
 end
