3ab1a3ce0faf1daa17f2e8324ed9569b344e3976
camron <camronmoradi@gmail.com>
2018-11-27 21:14:39 +0100

    Removes 'category' column from dishes table

---
+++ db/migrate/20181127201256_remove_category_from_dishes.rb
@@ -0,0 +1,5 @@
+class RemoveCategoryFromDishes < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :dishes, :category, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_184505) do
+ActiveRecord::Schema.define(version: 2018_11_27_201256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2018_11_26_184505) do
     t.string "name"
     t.text "description"
     t.integer "price"
-    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
