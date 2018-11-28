227bba53f264a574278c606b8de7c18ea8044c15
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-28 20:30:15 +0100

    Adds form to create new dish, adds create method to the controller, adds link to new dish on index page

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,4 +2,18 @@ class DishesController < ApplicationController
   def index
     @dishes = Dish.all
   end
+
+  def new
+    @dish = Dish.new
+  end
+
+  def create
+    @dish = Dish.create(dishes_params)
+    redirect_to dishes_path
+  end
+
+  private
+    def dishes_params
+        params.require(:dish).permit(:name, :description, :price)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users 
-  resources :dishes, only: [:index]
+  resources :dishes, only: [:index, :new, :create]
   root controller: :dishes, action: :index
 end
