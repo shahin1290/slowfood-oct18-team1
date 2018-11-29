611d3a8659d9d5af470e4ebb2ef83424638eee9e
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-29 20:48:55 +0100

    Adds method to read the yml file

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,5 +1,8 @@
 class RestaurantController < ApplicationController
-    def index
+    require 'yaml'
 
+    def index
+        file = Rails.root.join('app','assets','data','contactinfo.yml')
+        @contactinfo = YAML::load_file(file)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users 
   resources :dishes
+  resources :restaurant, only: :index
   root controller: :dishes, action: :index
 end
