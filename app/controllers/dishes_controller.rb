ab380907e401046351f37be6124cd39fe52d0e25
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-29 13:28:39 +0100

    Adds categories to add dish form, connects dishes to categories in the create method of the controller

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -5,10 +5,12 @@ class DishesController < ApplicationController
 
   def new
     @dish = Dish.new
+    @categories = Category.all
   end
 
   def create
     @dish = Dish.new(dishes_params)
+    @dish.update(category: Category.find_by(id: params[:dish][:category_id]))
     if @dish.save
       redirect_to dishes_path
     else
