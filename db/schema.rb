f5c8863628f094ed25fb5ad9ebd446896660e1b5
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-02 19:05:09 +0100

    Another possible solution for heroku deployment error, changing the production environment rb

--- db/schema.rb
+++ db/schema.rb
@@ -104,9 +104,9 @@ ActiveRecord::Schema.define(version: 2018_11_28_192230) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.integer "phone"
     t.string "name"
-    t.string "address"
     t.boolean "restaurant_owner", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
